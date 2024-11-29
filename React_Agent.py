e844920b7779024d2f75dd499172d0cbc66d1992
s3975993 <s3975993@student.rmit.edu.au>
2024-11-29 18:02:25 +1100

    Implement Agent Class

--- React_Agent.py
+++ React_Agent.py
@@ -33,3 +33,28 @@ chat_completion = client.chat.completions.create(
 
 # Print the response
 print(chat_completion.choices[0].message.content)
+
+
+class Agent:
+    def __init__(self,client,system):
+        self.client=client
+        self.system=system
+        self.messages=[]
+        if self.system is not None:
+            self.messages.append({'role':'system','content':self.system})
+        
+    def __call__(self,message):
+        if message:
+            self.messages.append({'role':'system','content':message})
+        result=self.execute()
+        self.messages.append({'role':'assistant','content':result})
+        return result
+    
+    def execute(self):
+        completion=client.chat.completions.create(
+        messages=self.messages,
+        model="llama3-70b-8192",
+        stream=False,
+        )
+        return completion.choices[0].message.content
+            
